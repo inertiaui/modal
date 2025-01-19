654ef073a755b1eda931d449a0b528ee0317d6a9
pascalbaljet <8403149+pascalbaljet@users.noreply.github.com>
2025-01-19 22:43:12 +0000

    Fix styling

--- react/src/ModalContent.jsx
+++ react/src/ModalContent.jsx
@@ -34,22 +34,18 @@ const ModalContent = ({ modalContext, config, children }) => {
                     leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                     afterEnter={afterEnter}
                     afterLeave={modalContext.afterLeave}
-                    className={clsx(
-                        'im-modal-wrapper w-full transition duration-300 ease-in-out',
-                        modalContext.onTopOfStack ? '' : 'blur-sm',
-                        {
-                            'sm:max-w-sm': config.maxWidth === 'sm',
-                            'sm:max-w-md': config.maxWidth === 'md',
-                            'sm:max-w-md md:max-w-lg': config.maxWidth === 'lg',
-                            'sm:max-w-md md:max-w-xl': config.maxWidth === 'xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-2xl': config.maxWidth === '2xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl': config.maxWidth === '3xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl': config.maxWidth === '4xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl': config.maxWidth === '5xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl': config.maxWidth === '6xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl': config.maxWidth === '7xl',
-                        },
-                    )}
+                    className={clsx('im-modal-wrapper w-full transition duration-300 ease-in-out', modalContext.onTopOfStack ? '' : 'blur-sm', {
+                        'sm:max-w-sm': config.maxWidth === 'sm',
+                        'sm:max-w-md': config.maxWidth === 'md',
+                        'sm:max-w-md md:max-w-lg': config.maxWidth === 'lg',
+                        'sm:max-w-md md:max-w-xl': config.maxWidth === 'xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-2xl': config.maxWidth === '2xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl': config.maxWidth === '3xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl': config.maxWidth === '4xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl': config.maxWidth === '5xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl': config.maxWidth === '6xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl': config.maxWidth === '7xl',
+                    })}
                 >
                     <div
                         className={`im-modal-content relative ${config.paddingClasses} ${config.panelClasses}`}
--- react/src/SlideoverContent.jsx
+++ react/src/SlideoverContent.jsx
@@ -33,22 +33,18 @@ const SlideoverContent = ({ modalContext, config, children }) => {
                     leaveTo={`opacity-0 ${config.position === 'left' ? '-translate-x-full' : 'translate-x-full'}`}
                     afterEnter={afterEnter}
                     afterLeave={modalContext.afterLeave}
-                    className={clsx(
-                        'im-slideover-wrapper w-full transition duration-300 ease-in-out',
-                        modalContext.onTopOfStack ? '' : 'blur-sm',
-                        {
-                            'sm:max-w-sm': config.maxWidth === 'sm',
-                            'sm:max-w-md': config.maxWidth === 'md',
-                            'sm:max-w-md md:max-w-lg': config.maxWidth === 'lg',
-                            'sm:max-w-md md:max-w-xl': config.maxWidth === 'xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-2xl': config.maxWidth === '2xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl': config.maxWidth === '3xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl': config.maxWidth === '4xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl': config.maxWidth === '5xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl': config.maxWidth === '6xl',
-                            'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl': config.maxWidth === '7xl',
-                        },
-                    )}
+                    className={clsx('im-slideover-wrapper w-full transition duration-300 ease-in-out', modalContext.onTopOfStack ? '' : 'blur-sm', {
+                        'sm:max-w-sm': config.maxWidth === 'sm',
+                        'sm:max-w-md': config.maxWidth === 'md',
+                        'sm:max-w-md md:max-w-lg': config.maxWidth === 'lg',
+                        'sm:max-w-md md:max-w-xl': config.maxWidth === 'xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-2xl': config.maxWidth === '2xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl': config.maxWidth === '3xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-4xl': config.maxWidth === '4xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl': config.maxWidth === '5xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-6xl': config.maxWidth === '6xl',
+                        'sm:max-w-md md:max-w-xl lg:max-w-3xl xl:max-w-5xl 2xl:max-w-7xl': config.maxWidth === '7xl',
+                    })}
                 >
                     <div
                         className={`im-slideover-content relative ${config.paddingClasses} ${config.panelClasses}`}
