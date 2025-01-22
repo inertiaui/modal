f28c1502146f996d15aa1c4a324b906b441a07e6
Pascal Baljet <pascal@protone.media>
2025-01-23 00:25:20 +0100

    Improve visiting same url (#86)
    * No interception of visits as we can't grab the events

--- demo-app/resources/js/Pages/EditUser.vue
+++ demo-app/resources/js/Pages/EditUser.vue
@@ -85,7 +85,7 @@ function onMessage(message) {
             </div>
 
             <div class="flex justify-end">
-                <Link :href="route('users.edit', { user, navigate: 1, randomKey })">
+                <Link replace :href="route('users.edit', { user, navigate: 1, randomKey })">
                     Edit again!
                 </Link>
                 <button type="button" @click="close" class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
