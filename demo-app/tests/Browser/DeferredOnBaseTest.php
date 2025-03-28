94f620289cf50a9de05233e3d474ddef302603de
Pascal Baljet <pascal@protone.media>
2025-03-28 10:03:26 +0100

    Added more tests + improvements for loading directly (#109)

--- demo-app/tests/Browser/DeferredOnBaseTest.php
+++ demo-app/tests/Browser/DeferredOnBaseTest.php
@@ -3,13 +3,46 @@
 namespace Tests\Browser;
 
 use App\Models\User;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\DuskTestCase;
 
 class DeferredOnBaseTest extends DuskTestCase
 {
+    #[DataProvider('booleanProvider')]
     #[Test]
-    public function it_can_perform_a_partial_request_on_a_base_url()
+    public function it_can_perform_a_partial_request_on_a_base_url(bool $navigate)
+    {
+        if (! $this->isInertiaV2()) {
+            return $this->markTestSkipped('Deferred Props are only available in Inertia v2');
+        }
+
+        $this->browse(function (Browser $browser) use ($navigate) {
+            $firstUser = User::orderBy('name')->first();
+
+            $browser->visit('/users'.($navigate ? '?navigate=1' : ''))
+                ->waitForFirstUser()
+                ->waitForTextIn('@deferred', 'Deferred data without Base URL header: page users')
+                ->click("@edit-user-{$firstUser->id}")
+                ->waitForModal()
+                ->waitForTextIn('@deferred', $navigate
+                    ? 'Deferred data with Base URL header: page users'
+                    : 'Deferred data without Base URL header: page users'
+                );
+
+            if ($navigate) {
+                $browser->assertPathIs('/users/'.$firstUser->id.'/edit');
+            }
+
+            $browser->clickModalCloseButton()
+                ->waitUntilMissingModal()
+                ->waitForTextIn('@deferred', 'Deferred data without Base URL header: page users')
+                ->assertPathIs('/users');
+        });
+    }
+
+    #[Test]
+    public function it_can_perform_a_partial_request_on_a_base_url_when_visiting_the_modal_url_directly()
     {
         if (! $this->isInertiaV2()) {
             return $this->markTestSkipped('Deferred Props are only available in Inertia v2');
@@ -18,12 +51,39 @@ class DeferredOnBaseTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $firstUser = User::orderBy('name')->first();
 
-            $browser->visit("/users/{$firstUser->id}")
+            $browser->visit("/users/{$firstUser->id}/edit")
                 ->waitForFirstUser()
+                ->waitForModal()
+                ->waitForTextIn('@deferred', 'Deferred data with Base URL header: page users')
+                ->assertPathIs('/users/'.$firstUser->id.'/edit')
+                ->clickModalCloseButton()
+                ->waitUntilMissingModal()
+                ->waitForTextIn('@deferred', 'Deferred data without Base URL header: page users')
+                ->assertPathIs('/users');
+        });
+    }
+
+    #[Test]
+    public function it_can_perform_a_partial_request_on_a_different_base_url()
+    {
+        if (! $this->isInertiaV2()) {
+            return $this->markTestSkipped('Deferred Props are only available in Inertia v2');
+        }
+
+        $this->browse(function (Browser $browser) {
+            $firstUser = User::orderBy('name')->first();
+
+            $browser->visit('/users/'.$firstUser->id)
+                ->waitForFirstUser()
+                ->waitForTextIn('@deferred', 'Deferred data without Base URL header: users.show')
                 ->click("@edit-user-{$firstUser->id}")
                 ->waitForModal()
-                ->waitForTextIn('@deferred', 'Deferred data')
-                ->assertPathIs('/users/'.$firstUser->id.'/edit');
+                ->waitForTextIn('@deferred', 'Deferred data with Base URL header: users.show')
+                ->assertPathIs('/users/'.$firstUser->id.'/edit')
+                ->clickModalCloseButton()
+                ->waitUntilMissingModal()
+                ->waitForTextIn('@deferred', 'Deferred data without Base URL header: users.show')
+                ->assertPathIs('/users/'.$firstUser->id);
         });
     }
 }
