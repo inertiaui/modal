4bff7c1c742a7312a4a130cb788d691d6f24dfe2
Pascal Baljet <pascal@protone.media>
2026-05-18 11:08:19 +0200

    Switch scripts (solo.yml, install-dev.sh, switch-vanilla-*.sh, release.sh, composer scripts) to pnpm workspace

--- switch-vanilla-npm.sh
+++ switch-vanilla-npm.sh
@@ -8,23 +8,18 @@ set -e
 cd "$(dirname "$0")"
 
 # Get latest version from npm or use provided version
-VERSION=${1:-$(npm view @inertiaui/vanilla version)}
+VERSION=${1:-$(pnpm view @inertiaui/vanilla version)}
 
 echo "Switching to npm vanilla (^$VERSION)..."
 
-# Update vue/package.json devDependencies only (first occurrence)
-sed -i '' "s|\"@inertiaui/vanilla\": \"file:../../vanilla\"|\"@inertiaui/vanilla\": \"^$VERSION\"|" vue/package.json
+# Root devDependencies
+sed -i '' "s|\"@inertiaui/vanilla\": \"link:[^\"]*\"|\"@inertiaui/vanilla\": \"^$VERSION\"|" package.json
 
-# Update react/package.json devDependencies only (first occurrence)
-sed -i '' "s|\"@inertiaui/vanilla\": \"file:../../vanilla\"|\"@inertiaui/vanilla\": \"^$VERSION\"|" react/package.json
+# vue + react (both occurrences each)
+sed -i '' "s|\"@inertiaui/vanilla\": \"link:[^\"]*\"|\"@inertiaui/vanilla\": \"^$VERSION\"|g" vue/package.json
+sed -i '' "s|\"@inertiaui/vanilla\": \"link:[^\"]*\"|\"@inertiaui/vanilla\": \"^$VERSION\"|g" react/package.json
 
-# Reinstall dependencies
-echo "Reinstalling vue dependencies..."
-cd vue && rm -rf node_modules package-lock.json && npm install
-cd ..
-
-echo "Reinstalling react dependencies..."
-cd react && rm -rf node_modules package-lock.json && npm install
-cd ..
+echo "Reinstalling workspace dependencies..."
+pnpm install
 
 echo "Done! Using npm vanilla@^$VERSION"
