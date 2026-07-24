4ea8537baf47e3b4cd332ffb4fde779d7bd2544f
Pascal Baljet <pascal@protone.media>
2026-07-24 23:44:53 +0200

    Favicon

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -138,22 +138,24 @@ for (const file of v3Files) {
     rewrites[`v3/${file}.md`] = `${file}.md`
 }
 
+const productionHead =
+    process.env.NODE_ENV === 'production'
+        ? [
+              [
+                  'script',
+                  {
+                      async: '',
+                      src: 'https://analytics.ahrefs.com/analytics.js',
+                      'data-key': 'A06g5iU8TH9IWlCPkl0/OQ',
+                  },
+              ],
+          ]
+        : []
+
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
     title: 'Inertia Modal Documentation',
-    head: [
-        process.env.NODE_ENV === 'production'
-            ? [
-                  'script',
-                  {
-                      defer: '',
-                      src: 'https://helloworld.protone.media/hey.js',
-                      'data-website-id': '553d745e-2115-4553-9d5d-bf4bc49052f5',
-                  },
-              ]
-            : [],
-        ['link', { rel: 'icon', href: '/favicon.ico' }],
-    ],
+    head: [...productionHead, ['link', { rel: 'icon', href: '/favicon.ico' }]],
     description: 'Documentation for the Inertia Modal package',
     base: process.env.NODE_ENV === 'production' ? '/inertia-modal/docs/' : null,
     cleanUrls: process.env.NODE_ENV === 'production',
